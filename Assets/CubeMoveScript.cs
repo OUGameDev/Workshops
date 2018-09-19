5ee9082c36dd4592154ebba9a289d0d84825dfc0
Noah Zemlin <noah.zemlin@ou.edu>
2018-09-18 20:50:46 -0500

    Workshop 2018-09-18 files

---
+++ Assets/CubeMoveScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMoveScript : MonoBehaviour {
+
+    Rigidbody rb;
+    bool holdingD = false;
+    bool holdingA = false;
+    bool holdingW = false;
+
+	// Use this for initialization
+	void Start () {
+
+        Debug.Log("Hello World!");
+
+        rb = this.GetComponent<Rigidbody>(); //Get the rigidbody associated with this object
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        holdingD = Input.GetKey("d");
+        holdingA = Input.GetKey("a");
+
+        //make holdingW true on the first frame that w is pressed
+        //keep it true until somewhere else sets it false
+        holdingW = Input.GetKeyDown("w") || holdingW;
+
+    }
+    // Update is called once per PHYSICS frame
+    void FixedUpdate()
+    {
+        if (holdingD == true)
+            rb.AddForce(new Vector3(20, 0, 0));
+
+        if (holdingA == true)
+            rb.AddForce(new Vector3(-20, 0, 0));
+
+        if (holdingW == true)
+        {
+            rb.AddForce(new Vector3(0, 300, 0));
+            holdingW = false; //After a jump is triggered, set holdingW back to false
+        }
+
+    }
+}
---
+++ Assets/EnemyCubeMoveScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCubeMoveScript : MonoBehaviour {
+
+    public GameObject player; //a variable containing the player's cube
+    public int speed; //just an example of how you can make a public speed variable that will show in the inspector
+
+    Rigidbody rb;
+    bool holdingD = false;
+    bool holdingA = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Debug.Log("Hello World!");
+
+        rb = this.GetComponent<Rigidbody>(); //Get the rigidbody associated with this object
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //make the bot hold D if the player is to the right, otherwise hold A
+        holdingD = player.transform.position.x > this.transform.position.x;
+        holdingA = player.transform.position.x < this.transform.position.x;
+    }
+    // Update is called once per PHYSICS frame
+    void FixedUpdate()
+    {
+        if (holdingD == true) //if holding D, make a force applying to the right
+            rb.AddForce(new Vector3(20, 0, 0));
+
+        if (holdingA == true) //if holding A, make a force applying to the left
+            rb.AddForce(new Vector3(-20, 0, 0));
+
+    }
+}
